3b91f8f978f565af4cda8a8eed063ccdbc763d11
NachoKai <47578311+NachoKai@users.noreply.github.com>
2020-01-04 19:03:56 -0300

    .

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ let listaPokemon = document.getElementById("lista-pokemon"),
     item = listaPokemon.querySelector(`#pokemon`),
     imagen = item.querySelector(`#poke-img`),
     imagenShiny = item.querySelector(`#poke-img-shiny`),
+    imagenBack = item.querySelector(`#poke-imgBack`),
+    imagenShinyBack = item.querySelector(`#poke-img-shinyBack`),
     nombre = item.querySelector(`#poke-name`),
     numero = item.querySelector(`#poke-number`),
     peso = item.querySelector(`#poke-weight`),
     altura = item.querySelector(`#poke-height`),
     tipoA = item.querySelector(`#poke-type-a`),
     tipoB = item.querySelector(`#poke-type-b`),
-    shinyBtn = item.querySelector(`#shinyBtn`)
+    shinyBtn = item.querySelector(`#shinyBtn`),
+    left = item.querySelector(`#leftBtn`),
+    right = item.querySelector(`#rightBtn`)
 
 function consultarPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -28,8 +32,16 @@ function consultarPokemones() {
 function crearPokemon(pokemon) {
     imagen.src = pokemon.sprites.front_default
     imagen.className = 'poke-img'
+
     imagenShiny.src = pokemon.sprites.front_shiny
     imagenShiny.className = 'hidden'
+
+    imagenShinyBack.src = pokemon.sprites.back_shiny
+    imagenShinyBack.className = 'hidden'
+
+    imagenBack.src = pokemon.sprites.back_default
+    imagenBack.className = 'hidden'
+
     nombre.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
     numero.textContent = `#${pokemon.id}`
     peso.textContent = `Weight: ${Math.round((pokemon.weight)/10)} kg.`
@@ -49,13 +61,47 @@ function crearPokemon(pokemon) {
 }
 
 function shinyBtnFn() {
-    if (imagenShiny.className === 'hidden') {
-        imagenShiny.className = 'poke-img'
-        imagen.className = 'hidden'
-    } else {
-        imagenShiny.className = 'hidden'
-        imagen.className = 'poke-img'
+    if (imagen.className === 'poke-img') {
+        muestraShiny()
+    }else{
+        muestraDefault()
     }
+}
+
+function turnFn() {
+    if (imagen.className === 'poke-img') {
+        muestraDefaultBack()
+    }else{
+        muestraDefault()
+    }
+}
+
+function muestraDefault() {
+    imagen.className = 'poke-img'
+    imagenBack.className = 'hidden'
+    imagenShiny.className = 'hidden'
+    imagenShinyBack.className = 'hidden'
+}
+
+function muestraShiny() {
+    imagen.className = 'hidden'
+    imagenBack.className = 'hidden'
+    imagenShiny.className = 'poke-img'
+    imagenShinyBack.className = 'hidden'
+}
+
+function muestraDefaultBack() {
+    imagen.className = 'hidden'
+    imagenBack.className = 'poke-img'
+    imagenShiny.className = 'hidden'
+    imagenShinyBack.className = 'hidden'
+}
+
+function muestraShinyBack() {
+    imagen.className = 'hidden'
+    imagenBack.className = 'hidden'
+    imagenShiny.className = 'hidden'
+    imagenShinyBack.className = 'poke-img'
 }
 
 /*
